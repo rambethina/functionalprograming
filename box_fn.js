e1075334f3508a67aeff85fa03e16351624f8ba9
vrbethi <rambabu.bethina@monsanto.com>
2017-02-12 10:27:25 -0600

    Create linear data flow with container style types (Box)

---
+++ box_fn.js
@@ -0,0 +1,26 @@
+// const nextCharForNumberString = str => {
+//   const trimmed = str.trim()
+//   const number = parseInt(trimmed)
+//   const nextNumber = number + 1
+//   return String.fromCharCode(nextNumber)
+// }
+
+const Box = x =>
+({
+  fold: f => f(x),
+  map: f => Box(f(x)),           //Identity functor
+  inspect: () => `Box(${x})`
+})
+
+const nextCharForNumberString = str =>
+  Box(str)
+  .map(s => s.trim())
+  .map(s => parseInt(s))
+  .map(i => i + 1)
+  .map(i => String.fromCharCode(i))
+  .fold(c => c.toLowerCase())
+
+
+const result = nextCharForNumberString(' 64 ')
+
+console.log(result)
